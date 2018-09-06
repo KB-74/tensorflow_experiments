c23a662c9559fef766cc0f57633b49d2aa23de0b
Job Vink <jmcvink@gmail.com>
2018-09-06 22:48:59 +0200

    added comments

--- basic_classification.py
+++ basic_classification.py
@@ -8,35 +8,36 @@ from tensorflow import keras
 fashion_mnist = keras.datasets.fashion_mnist
 
 # Verdeel de dataset in een trainings set en een test set.
+# de trainings set heeft 60000 records en de test set heeft er 10000
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-print('shape of training set is: ' + str(train_images.shape))
-print('shape of test set is: ' + str(train_images.shape))
-'''
+# delen door 255 zodat de greysclae pixelwaredes nu tussen de 0 en 1 zitten
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+print('shape of training set is: ' + str(train_images.shape))
+print('shape of test set is: ' + str(test_images.shape))
+
+# bijbehoorende catahorie namen
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-print('The shape of the data set is ' + str(train_images.shape))
-
+# Computational machine learning model met twee hidden layers
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
+# model compileren
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+# het trainene van het machine learning model
 model.fit(train_images, train_labels, epochs=5)
 
+# getting all the metrics
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
-
-# maken van voorspellingen
-predictions = model.predict(test_images)
-'''
